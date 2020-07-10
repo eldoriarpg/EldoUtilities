0d9a69f90378deeb7068497867851529ffa35302
RainbowDashLabs <mail@code-fox.de>
2020-07-10 13:47:33 +0200

    add pair and tripple container

---
+++ src/main/java/de/eldoria/eldoutilities/container/Triple.java
@@ -0,0 +1,13 @@
+package de.eldoria.eldoutilities.container;
+
+public class Triple<A, B, C> {
+    public final A first;
+    public final B second;
+    public final C third;
+
+    public Triple(A first, B second, C third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+}
