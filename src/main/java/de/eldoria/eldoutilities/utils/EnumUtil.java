9d6c7db3dd033e7551968104cd0f64f298d717c0
RainbowDashLabs <mail@code-fox.de>
2020-07-10 13:46:58 +0200

    add Enum Utils for easier enum parsing

---
+++ src/main/java/de/eldoria/eldoutilities/utils/EnumUtil.java
@@ -0,0 +1,37 @@
+package de.eldoria.eldoutilities.utils;
+
+public final class EnumUtil {
+    private EnumUtil() {
+        throw new UnsupportedOperationException("This is a utility class!");
+    }
+
+    /**
+     * Searches for a enum value by string with a case insensitive search.
+     *
+     * @param string enum as string value
+     * @param values enum values.
+     * @param <T>    type of enum.
+     * @return enum value or null if no mathing value was found.
+     */
+    public static <T extends Enum<T>> T parse(String string, Class<T> values) {
+        return parse(string, values, false);
+    }
+
+    /**
+     * Searches for a enum value by string with a case insensitive search.
+     *
+     * @param string       enum as string value
+     * @param values       enum values.
+     * @param stripStrings if true underscores will be removed before checking
+     * @param <T>          type of enum.
+     * @return enum value or null if no mathing value was found.
+     */
+    public static <T extends Enum<T>> T parse(String string, Class<T> values, boolean stripStrings) {
+        for (T value : values.getEnumConstants()) {
+            if (string.equalsIgnoreCase(stripStrings ? value.name().replace("_", "") : value.name())) {
+                return value;
+            }
+        }
+        return null;
+    }
+}
