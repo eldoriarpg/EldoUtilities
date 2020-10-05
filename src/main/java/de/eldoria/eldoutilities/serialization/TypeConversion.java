7695bb13c263d6068d016eade24434be410d96dd
RainbowDashLabs <mail@code-fox.de>
2020-10-05 17:02:11 +0200

    add type conversion

---
+++ src/main/java/de/eldoria/eldoutilities/serialization/TypeConversion.java
@@ -0,0 +1,25 @@
+package de.eldoria.eldoutilities.serialization;
+
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+public final class TypeConversion {
+    private TypeConversion() {
+    }
+
+    public static byte[] getBytesFromUUID(UUID uuid) {
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+
+        return bb.array();
+    }
+
+    public static UUID getUUIDFromBytes(byte[] bytes) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+        Long high = byteBuffer.getLong();
+        Long low = byteBuffer.getLong();
+
+        return new UUID(high, low);
+    }
+}
