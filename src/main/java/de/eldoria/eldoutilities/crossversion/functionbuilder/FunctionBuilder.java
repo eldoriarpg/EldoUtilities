6822f82864fa55eddd2f291deefb111431ffe492
RainbowDashLabs <mail@code-fox.de>
2020-10-05 17:01:14 +0200

    add function builder

--- src/main/java/de/eldoria/eldoutilities/crossversion/functionbuilder/FunctionBuilder.java
+++ src/main/java/de/eldoria/eldoutilities/crossversion/functionbuilder/FunctionBuilder.java
@@ -10,19 +10,42 @@ import java.util.function.Function;
 public class FunctionBuilder<A, R> {
     private final Map<ServerVersion, Function<A, R>> functions = new EnumMap<>(ServerVersion.class);
 
-    public FunctionBuilder<A, R> addVersionFunction(Function<A,  R> function, ServerVersion... version) {
+    protected FunctionBuilder() {
+    }
+
+    /**
+     * Add a version function which should be used on one or more versions.
+     *
+     * @param function function to execute
+     * @param version  versions which should use this function
+     * @return builder instance with function applied for versions
+     */
+    public FunctionBuilder<A, R> addVersionFunction(Function<A, R> function, ServerVersion... version) {
         for (ServerVersion serverVersion : version) {
             functions.put(serverVersion, function);
         }
         return this;
     }
 
-    public FunctionBuilder<A, R> addVersionFunctionBetween(ServerVersion newest, ServerVersion oldest, Function<A,R> function) {
+    /**
+     * Add a version functions for all versions between two versions.
+     *
+     * @param oldest   oldest version (inclusive)
+     * @param newest   newest version (inclusive)
+     * @param function function to execute
+     * @return builder instance with function applied for versions
+     */
+    public FunctionBuilder<A, R> addVersionFunctionBetween(ServerVersion oldest, ServerVersion newest, Function<A, R> function) {
         addVersionFunction(function, ServerVersion.versionsBetween(oldest, newest));
         return this;
     }
 
-    public VersionFunction<A,R> build(){
+    /**
+     * Build the version function.
+     *
+     * @return version functions with applied functions for versions.
+     */
+    public VersionFunction<A, R> build() {
         return new VersionFunction<>(functions);
     }
 }
--- src/main/java/de/eldoria/eldoutilities/crossversion/functionbuilder/QuadFunctionBuilder.java
+++ src/main/java/de/eldoria/eldoutilities/crossversion/functionbuilder/QuadFunctionBuilder.java
@@ -1,7 +1,7 @@
 package de.eldoria.eldoutilities.crossversion.functionbuilder;
 
-import de.eldoria.eldoutilities.crossversion.function.QuadVersionFunction;
 import de.eldoria.eldoutilities.crossversion.ServerVersion;
+import de.eldoria.eldoutilities.crossversion.function.QuadVersionFunction;
 import de.eldoria.eldoutilities.functions.QuadFunction;
 
 import java.util.EnumMap;
@@ -10,6 +10,16 @@ import java.util.Map;
 public class QuadFunctionBuilder<A, B, C, D, R> {
     private final Map<ServerVersion, QuadFunction<A, B, C, D, R>> functions = new EnumMap<>(ServerVersion.class);
 
+    protected QuadFunctionBuilder() {
+    }
+
+    /**
+     * Add a version function which should be used on one or more versions.
+     *
+     * @param function function to execute
+     * @param version  versions which should use this function
+     * @return builder instance with function applied for versions
+     */
     public QuadFunctionBuilder<A, B, C, D, R> addVersionFunction(QuadFunction<A, B, C, D, R> function, ServerVersion... version) {
         for (ServerVersion serverVersion : version) {
             functions.put(serverVersion, function);
@@ -17,12 +27,25 @@ public class QuadFunctionBuilder<A, B, C, D, R> {
         return this;
     }
 
+    /**
+     * Add a version functions for all versions between two versions.
+     *
+     * @param oldest   oldest version (inclusive)
+     * @param newest   newest version (inclusive)
+     * @param function function to execute
+     * @return builder instance with function applied for versions
+     */
     public QuadFunctionBuilder<A, B, C, D, R> addVersionFunctionBetween(ServerVersion oldest, ServerVersion newest, QuadFunction<A, B, C, D, R> function) {
         addVersionFunction(function, ServerVersion.versionsBetween(oldest, newest));
         return this;
     }
 
-    public QuadVersionFunction<A,B,C,D,R> build(){
+    /**
+     * Build the version function.
+     *
+     * @return version functions with applied functions for versions.
+     */
+    public QuadVersionFunction<A, B, C, D, R> build() {
         return new QuadVersionFunction<>(functions);
     }
 }
