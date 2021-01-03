c42e55a34b0d7fdf7173cf874ba906e4f2ce8304
Florian Fülling <mail@chojo.de>
2021-01-03 23:40:06 +0100

    Migrate EldoUtilities to make it usable as a plugin

---
+++ src/main/java/de/eldoria/eldoutilities/EldoUtilities.java
@@ -0,0 +1,13 @@
+package de.eldoria.eldoutilities;
+
+import de.eldoria.eldoutilities.plugin.EldoPlugin;
+
+public final class EldoUtilities extends EldoPlugin {
+    private EldoUtilities() {
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+}
