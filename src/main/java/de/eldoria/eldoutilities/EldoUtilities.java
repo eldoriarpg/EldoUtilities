31fd84f55e8d7fa75972eec24a43e2028b5de1f4
Florian Fülling <mail@chojo.de>
2021-01-08 00:25:07 +0100

    make eldo utilitites a plugin

--- src/main/java/de/eldoria/eldoutilities/EldoUtilities.java
+++ src/main/java/de/eldoria/eldoutilities/EldoUtilities.java
@@ -5,15 +5,12 @@ import de.eldoria.eldoutilities.plugin.EldoPlugin;
 import java.util.logging.Logger;
 
 public final class EldoUtilities extends EldoPlugin {
-    private EldoUtilities() {
-    }
-
     @Override
     public void onEnable() {
 
     }
 
-    public static Logger logger(){
+    public static Logger logger() {
         return getInstance(EldoUtilities.class).getLogger();
     }
 }
