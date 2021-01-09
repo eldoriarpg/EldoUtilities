352c6504a68e40c84ab5733cde42a30d3fd66b70
Florian Fülling <mail@chojo.de>
2021-01-09 22:54:50 +0100

    register some utilities for general usage

--- src/main/java/de/eldoria/eldoutilities/EldoUtilities.java
+++ src/main/java/de/eldoria/eldoutilities/EldoUtilities.java
@@ -1,13 +1,33 @@
 package de.eldoria.eldoutilities;
 
+import de.eldoria.eldoutilities.inventory.InventoryActionHandler;
 import de.eldoria.eldoutilities.plugin.EldoPlugin;
+import de.eldoria.eldoutilities.scheduling.DelayedActions;
 
 import java.util.logging.Logger;
 
 public final class EldoUtilities extends EldoPlugin {
+    private static DelayedActions delayedActions;
+    private static InventoryActionHandler inventoryActionHandler;
+
+
+    public static DelayedActions getDelayedActions() {
+        return delayedActions;
+    }
+
+    public static InventoryActionHandler getInventoryActions(){
+        return inventoryActionHandler;
+    }
+
+    @Override
+    public void onLoad() {
+    }
+
     @Override
     public void onEnable() {
-
+        delayedActions = DelayedActions.start(this);
+        inventoryActionHandler = new InventoryActionHandler();
+        registerListener(inventoryActionHandler);
     }
 
     public static Logger logger() {
