758fccf9e30b44749c4827824dd98135b68caef5
Florian Fülling <mail@chojo.de>
2021-01-10 02:20:07 +0100

    add bstats

--- src/main/java/de/eldoria/eldoutilities/core/EldoUtilities.java
+++ src/main/java/de/eldoria/eldoutilities/core/EldoUtilities.java
@@ -1,14 +1,18 @@
-package de.eldoria.eldoutilities;
+package de.eldoria.eldoutilities.core;
 
 import de.eldoria.eldoutilities.inventory.InventoryActionHandler;
 import de.eldoria.eldoutilities.plugin.EldoPlugin;
 import de.eldoria.eldoutilities.scheduling.DelayedActions;
+import de.eldoria.eldoutilities.updater.Updater;
+import de.eldoria.eldoutilities.updater.butlerupdater.ButlerUpdateData;
+import org.bstats.bukkit.Metrics;
 
 import java.util.logging.Logger;
 
 public final class EldoUtilities extends EldoPlugin {
     private static DelayedActions delayedActions;
     private static InventoryActionHandler inventoryActionHandler;
+    private Configuration configuration;
 
 
     public static DelayedActions getDelayedActions() {
@@ -29,8 +33,15 @@ public final class EldoUtilities extends EldoPlugin {
 
     @Override
     public void onEnable() {
+        configuration = new Configuration(this);
         delayedActions = DelayedActions.start(this);
         inventoryActionHandler = new InventoryActionHandler();
         registerListener(inventoryActionHandler);
+        Updater.Butler(new ButlerUpdateData(this, "eldoutilities", configuration.isUpdateCheck(),
+                false, 9, ButlerUpdateData.HOST)).start();
+        Metrics metrics = new Metrics(this, 9958);
+        if(metrics.isEnabled()){
+            getLogger().info("§2Metrics enabled. Thank you <3");
+        }
     }
 }
