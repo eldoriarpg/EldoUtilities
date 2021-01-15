efccdda2e2b8af2360f2fc8bed9a61d9ecb1b106
Florian Fülling <mail@chojo.de>
2021-01-15 14:54:04 +0100

    rework ConfigDumpData as EntryData

---
+++ src/main/java/de/eldoria/eldoutilities/debug/data/EntryData.java
@@ -0,0 +1,11 @@
+package de.eldoria.eldoutilities.debug.data;
+
+public class EntryData {
+    protected String name;
+    protected String content;
+
+    public EntryData(String name, String content) {
+        this.name = name;
+        this.content = content;
+    }
+}
