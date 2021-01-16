091dad887e547c44c12b936c71d6a0f69d5397b4
Florian Fülling <mail@chojo.de>
2021-01-16 23:52:24 +0100

    add lazy init.

--- src/main/java/de/eldoria/eldoutilities/core/EldoUtilities.java
+++ src/main/java/de/eldoria/eldoutilities/core/EldoUtilities.java
@@ -2,26 +2,49 @@ package de.eldoria.eldoutilities.core;
 
 import de.eldoria.eldoutilities.core.commands.EldoDebug;
 import de.eldoria.eldoutilities.inventory.InventoryActionHandler;
+import de.eldoria.eldoutilities.messages.MessageChannel;
 import de.eldoria.eldoutilities.messages.MessageSender;
 import de.eldoria.eldoutilities.plugin.EldoPlugin;
+import de.eldoria.eldoutilities.scheduling.AsyncSyncingCallbackExecutor;
 import de.eldoria.eldoutilities.scheduling.DelayedActions;
 import de.eldoria.eldoutilities.updater.Updater;
 import de.eldoria.eldoutilities.updater.butlerupdater.ButlerUpdateData;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.boss.KeyedBossBar;
 
+import java.util.Iterator;
 import java.util.logging.Logger;
 
 public final class EldoUtilities extends EldoPlugin {
-    private static DelayedActions delayedActions;
-    private static InventoryActionHandler inventoryActionHandler;
+    private static DelayedActions delayedActions = null;
+    private static InventoryActionHandler inventoryActionHandler = null;
+    private static AsyncSyncingCallbackExecutor asyncSyncingCallbackExecutor = null;
     private Configuration configuration;
 
     public static DelayedActions getDelayedActions() {
+        if (delayedActions == null) {
+            delayedActions = DelayedActions.start(getInstance(EldoUtilities.class));
+            logger().config("DelayedActions ignited.");
+        }
         return delayedActions;
     }
 
     public static InventoryActionHandler getInventoryActions() {
+        if (inventoryActionHandler == null) {
+            inventoryActionHandler = InventoryActionHandler.create(getInstance(EldoUtilities.class));
+            logger().config("InventoryActionHandler ignited.");
+        }
         return inventoryActionHandler;
+    }
+
+    public static AsyncSyncingCallbackExecutor getAsyncSyncingCallbackExecutor() {
+        if (asyncSyncingCallbackExecutor == null) {
+            asyncSyncingCallbackExecutor = AsyncSyncingCallbackExecutor.create(getInstance(EldoUtilities.class));
+            logger().config("AsyncSyncingCallbackExecutor ignited.");
+        }
+        return asyncSyncingCallbackExecutor;
     }
 
     public static Logger logger() {
@@ -35,9 +58,7 @@ public final class EldoUtilities extends EldoPlugin {
     @Override
     public void onEnable() {
         configuration = new Configuration(this);
-        registerPublicServices();
         MessageSender.create(this, "§6[EU]");
-        registerListener(inventoryActionHandler);
         Updater.Butler(new ButlerUpdateData(this, "eldoutilities", configuration.isUpdateCheck(),
                 false, 9, ButlerUpdateData.HOST)).start();
         Metrics metrics = new Metrics(this, 9958);
@@ -47,10 +68,36 @@ public final class EldoUtilities extends EldoPlugin {
 
         registerCommand("eldoDebug", new EldoDebug(this));
         getLogger().info("EldoUtilities armed and ready.");
+        getScheduler().runTaskLater(this, this::performLateCleanUp, 5);
     }
 
-    private static void registerPublicServices() {
-        delayedActions = DelayedActions.start(getInstance(EldoUtilities.class));
+    private void performLateCleanUp() {
+        Iterator<KeyedBossBar> bossBars = Bukkit.getBossBars();
+        while (bossBars.hasNext()) {
+            KeyedBossBar bar = bossBars.next();
+            NamespacedKey key = bar.getKey();
+            if (!key.getNamespace().equalsIgnoreCase(getName())) continue;
+            if (key.getKey().startsWith(MessageChannel.KEY_PREFIX)) {
+                logger().config("Removed boss bar with key" + key.toString());
+                bar.removeAll();
+                Bukkit.removeBossBar(key);
+            }
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (delayedActions != null) {
+            delayedActions.shutdown();
+        }
+        if(asyncSyncingCallbackExecutor!= null){
+            asyncSyncingCallbackExecutor.shutdown();
+        }
+    }
+
+    private static void registerPublicServices(EldoUtilities plugin) {
+        delayedActions = DelayedActions.start(plugin);
         inventoryActionHandler = new InventoryActionHandler();
+        asyncSyncingCallbackExecutor = AsyncSyncingCallbackExecutor.create(plugin);
     }
 }
