89505e33b0fb5abed4d30a1e04f13f65c9e1cb1d
Florian Fülling <mail@chojo.de>
2021-01-16 03:53:40 +0100

    add channel data

--- src/main/java/de/eldoria/eldoutilities/messages/channeldata/ChannelData.java
+++ src/main/java/de/eldoria/eldoutilities/messages/channeldata/ChannelData.java
@@ -1,4 +1,12 @@
 package de.eldoria.eldoutilities.messages.channeldata;
 
+import de.eldoria.eldoutilities.localization.ILocalizer;
+import de.eldoria.eldoutilities.localization.Replacement;
+import de.eldoria.eldoutilities.messages.MessageType;
+
 public interface ChannelData {
+    default void localized(ILocalizer localizer, Replacement... replacements) {
+    }
+    default void forceColor(MessageType type) {
+    }
 }
--- src/main/java/de/eldoria/eldoutilities/messages/channeldata/TitleData.java
+++ src/main/java/de/eldoria/eldoutilities/messages/channeldata/TitleData.java
@@ -1,15 +1,43 @@
 package de.eldoria.eldoutilities.messages.channeldata;
 
-public class TitleData implements ChannelData {
-    public static final TitleData DEFAULT = new TitleData(10, 50, 20);
+import de.eldoria.eldoutilities.localization.ILocalizer;
+import de.eldoria.eldoutilities.localization.Replacement;
+import de.eldoria.eldoutilities.messages.MessageType;
+
+public final class TitleData implements ChannelData {
+    public static final TitleData DEFAULT = new TitleData(10, 50, 20, "");
     private final int fadeIn;
     private final int stay;
     private final int fadeOut;
+    private String otherLine;
 
-    public TitleData(int fadeIn, int stay, int fadeOut) {
+    public static TitleData forTime(int fadeIn, int stay, int fadeOut) {
+        return new TitleData(fadeIn, stay, fadeOut, "");
+    }
+
+    public static TitleData forOtherLine(String otherLine) {
+        return new TitleData(10, 50, 20, otherLine);
+    }
+
+    public static TitleData forFadeAndTime(int fadeIn, int stay, int fadeOut, String otherLine) {
+        return new TitleData(fadeIn, stay, fadeOut, otherLine);
+    }
+
+    @Override
+    public void localized(ILocalizer localizer, Replacement... replacements) {
+        otherLine = localizer.localize(otherLine, replacements);
+    }
+
+    @Override
+    public void forceColor(MessageType type) {
+        type.forceColor(otherLine);
+    }
+
+    private TitleData(int fadeIn, int stay, int fadeOut, String otherLine) {
         this.fadeIn = fadeIn;
         this.stay = stay;
         this.fadeOut = fadeOut;
+        this.otherLine = otherLine;
     }
 
     public int getFadeIn() {
@@ -22,5 +50,9 @@ public class TitleData implements ChannelData {
 
     public int getFadeOut() {
         return fadeOut;
+    }
+
+    public String getOtherLine() {
+        return otherLine;
     }
 }
