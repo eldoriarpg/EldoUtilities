8a24f00bfa3c09caef80629f77414ff3473804d6
Florian Fülling <mail@chojo.de>
2021-01-21 14:56:42 +0100

    add documentation

--- src/main/java/de/eldoria/eldoutilities/debug/data/EntryData.java
+++ src/main/java/de/eldoria/eldoutilities/debug/data/EntryData.java
@@ -4,6 +4,12 @@ public class EntryData {
     protected String name;
     protected String content;
 
+    /**
+     * Create a new debug entry
+     *
+     * @param name    Name of debug entry. This is the name displayed on the web page
+     * @param content content of the debug entry.
+     */
     public EntryData(String name, String content) {
         this.name = name;
         this.content = content;
