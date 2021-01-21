f9274da8fe366313345ed4db6bcaf299c23e2484
Florian Fülling <mail@chojo.de>
2021-01-21 14:57:40 +0100

    Add self scheduling worker

---
+++ src/main/java/de/eldoria/eldoutilities/scheduling/SelfSchedulingWorker.java
@@ -0,0 +1,97 @@
+package de.eldoria.eldoutilities.scheduling;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayDeque;
+import java.util.Collection;
+
+public abstract class SelfSchedulingWorker<V, T extends Collection<V>> extends BukkitRunnable {
+    private static final int MAX_DURATION_TARGET = 50; // assuming 50ms = 1 tick
+    private final Plugin plugin;
+    private final Collection<V> tasks;
+    private boolean running = false;
+    private int idleTicks = 0;
+    private int maxIdleTicks = 200;
+    private boolean active = true;
+
+    public SelfSchedulingWorker(Plugin plugin, int maxIdleTicks) {
+        this(plugin);
+        this.maxIdleTicks = maxIdleTicks;
+    }
+
+    public SelfSchedulingWorker(Plugin plugin) {
+        this.plugin = plugin;
+        tasks = getQueueImplementation();
+    }
+
+    /**
+     * handle one object which was polled from the queue
+     *
+     * @param object object from queue
+     */
+    protected abstract void execute(V object);
+
+    /**
+     * Tick is executed once per tick.
+     */
+    protected void tick() {
+    }
+
+    @Override
+    public final void run() {
+        if (!tasks.isEmpty()) {
+            tick();
+            for (V task : tasks) {
+                execute(task);
+            }
+        } else {
+            idleTicks++;
+            if (idleTicks >= maxIdleTicks) {
+                cancel();
+                plugin.getLogger().fine(getClass().getSimpleName() + " of " + plugin.getName() + " paused. No tasks left.");
+                running = false;
+            }
+        }
+    }
+
+    protected final void register(V object) {
+        if (!active) return;
+        tasks.add(object);
+        if (!running) {
+            runTaskTimer(plugin, 0, 1);
+            running = true;
+            idleTicks = 0;
+            plugin.getLogger().fine(getClass().getSimpleName() + " of " + plugin.getName() + " started. Processing tasks.");
+        }
+    }
+
+    public final void unregister(V object) {
+        tasks.remove(object);
+    }
+
+    protected Collection<V> getQueueImplementation() {
+        return new ArrayDeque<>();
+    }
+
+    public final void shutdown() {
+        cancel();
+        active = false;
+        for (V task : tasks) {
+            execute(task);
+        }
+        tasks.clear();
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
